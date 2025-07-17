1b6b8e193a94f28d59e9d84f38ec797b5be04362
0xcodemojo <tech.codemojo@gmail.com>
2025-07-17 03:16:02 +0100

    a

---
+++ app/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="text-center text-gray-500 text-sm py-4 border-t border-gray-800 mt-8">
+      QUIZ3. made with ❤️ by <a href="x.com/0xebby">0xebby</a>
+    </footer>
+  )
+}
